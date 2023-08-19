7a37ca771e518041e11c26bad643cb02e0a42d4c
SiriusBrightstar <siriusbrightstar@protonmail.com>
2023-08-19 15:54:26 +0530

    Working Code with Holidays

---
+++ channel_read_only.py
@@ -0,0 +1,60 @@
+import os
+from datetime import date
+from discord import Client
+from discord import Intents
+from dotenv import dotenv_values
+from nsepython import nse_holidays
+from pandas import json_normalize as jn
+
+cred = dotenv_values()
+
+# Set up the Discord client
+intents = Intents.default()
+intents.members = True  # To get member updates
+client = Client(intents=intents)
+
+
+def is_market_closed_today() -> list:
+    # Function that returns a list:
+    # [bool is_market_closed_today, if true: string reason]
+    pd_holidays = jn(nse_holidays('trading')['FO'])
+    today = date.today().strftime('%d-%b-%Y')
+    is_holiday = today in pd_holidays['tradingDate'].values
+
+    if is_holiday:
+        reason_row = pd_holidays.loc[pd_holidays['tradingDate']
+                                     == today]
+        reason = reason_row['description'].to_list()[0]
+        print(f'Holiday Reason: {reason}')
+        return [True, reason]
+    else:
+        print('Not a Holiday')
+        return [False]
+
+
+async def make_channel_read_only():
+    # Function to change channel permissions to read-only
+    channel = client.get_channel(int(cred['DISCORD_CHANNEL_ID']))
+
+    holiday = is_market_closed_today()
+    if holiday[0] == False:
+        read_only_role = channel.guild.get_role(int(cred['READ_ONLY_ROLE_ID']))
+        await channel.send(f'This Channel is locked till 15:30IST. Go to <#{int(cred["OPEN_CHANNEL_ID"])}>')
+        # Set read-only permission for the read-only role
+        await channel.set_permissions(read_only_role, read_messages=True, send_messages=False)
+        print(f"Channel {channel.name} is now read-only.")
+    else:
+        print('Today is a Holiday')
+        await channel.send(f'Markets are Closed today\nHappy {holiday[1]}')
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name} ({client.user.id})')
+    print('------')
+    await make_channel_read_only()
+
+    os._exit(0)
+
+# Run the Discord client
+client.run(cred['DISCORD_TOKEN'])
--- channel_read_write.py
+++ channel_read_write.py
@@ -11,21 +11,11 @@ intents.members = True  # To get member updates
 client = Client(intents=intents)
 
 
-async def make_channel_read_only():
-    # Function to change channel permissions to read-only
-    channel = client.get_channel(int(cred['DISCORD_CHANNEL_ID']))
-    read_only_role = channel.guild.get_role(int(cred['READ_ONLY_ROLE_ID']))
-    await channel.send('This Channel is locked till 15:30IST. Go to <#1053640650094034955>')
-    # Set read-only permission for the read-only role
-    await channel.set_permissions(read_only_role, read_messages=True, send_messages=False)
-    print(f"Channel {channel.name} is now read-only.")
-
-
 async def make_channel_read_write():
     # Function to change channel permissions to read & write
     channel = client.get_channel(int(cred['DISCORD_CHANNEL_ID']))
     read_only_role = channel.guild.get_role(int(cred['READ_ONLY_ROLE_ID']))
-
+    await channel.send('This Channel is now unlocked')
     # Set read-only permission for the read-only role
     await channel.set_permissions(read_only_role, read_messages=True, send_messages=True)
     print(f"Channel {channel.name} is now read & write.")
@@ -35,9 +25,7 @@ async def make_channel_read_write():
 async def on_ready():
     print(f'Logged in as {client.user.name} ({client.user.id})')
     print('------')
-    # Select which ever function is required
-    await make_channel_read_only()
-    # await make_channel_read_write()
+    await make_channel_read_write()
 
     os._exit(0)
 
